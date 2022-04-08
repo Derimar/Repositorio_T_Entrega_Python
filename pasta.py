87465397dc4d248887d827eed140a8be298f4e88
Derimar <38360727+Derimar@users.noreply.github.com>
2022-04-08 15:05:50 -0400

    usando o comando while no python

---
+++ pasta.py
@@ -0,0 +1,73 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "Untitled3.ipynb",
+      "provenance": [],
+      "collapsed_sections": [],
+      "authorship_tag": "ABX9TyNCmilgRXj2r9Pt50or6l5T",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Derimar/Repositorio_T_Entrega_Python/blob/main/pasta.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 1,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "wohs2wuMiYeE",
+        "outputId": "07229442-4dae-4fa9-8137-1e49dc284f53"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "ainda não deu\n",
+            "ainda não deu\n",
+            "ainda não deu\n",
+            "ainda não deu\n",
+            "ainda não deu\n",
+            "ainda não deu\n",
+            "ainda não deu\n",
+            "ainda não deu\n",
+            "ainda não deu\n",
+            "ainda não deu\n",
+            "agora deu\n"
+          ]
+        }
+      ],
+      "source": [
+        "import time\n",
+        "\n",
+        "contador = 0\n",
+        "while contador < 10:\n",
+        "    print ('ainda não deu')\n",
+        "    contador = contador +1\n",
+        "    time.sleep(1)\n",
+        "print('agora deu')\n",
+        "  "
+      ]
+    }
+  ]
+}
